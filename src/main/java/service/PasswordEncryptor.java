0821da1210bde9986b7e3bf4274c7413500b655f
oktay-osman <oktai.osman21@gmail.com>
2024-03-08 15:33:54 +0200

    Added PasswordEncryptor service

---
+++ src/main/java/service/PasswordEncryptor.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class PasswordEncryptor {
+    public static String encryptPassword(String password) {
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
