02f6e4543a295d79df4fcfc0e9c2c355251a87c0
oktay-osman <oktai.osman21@gmail.com>
2024-03-16 20:26:42 +0200

    Added Login controller

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+
+    @FXML
+    private ImageView brandingImageView;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private PasswordField enterPasswordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label loginMessageLabel;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void cancelButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginButtonOnAction(ActionEvent event) {
+
+    }
+
+}
