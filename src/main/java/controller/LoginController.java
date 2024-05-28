9c0673a38173d9803217bd0e329efaed5fb3f4ac
oktay-osman <oktai.osman21@gmail.com>
2024-05-29 00:49:14 +0300

    added initialization and loginButtonOnAction, changed access modifiers

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,40 +2,65 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private ImageView brandingImageView;
-
-    @FXML
-    private Button cancelButton;
-
-    @FXML
-    private PasswordField enterPasswordField;
+public class LoginController implements Initializable {
 
     @FXML
-    private Button loginButton;
+    public ImageView brandingImageView;
 
     @FXML
-    private Label loginMessageLabel;
+    public Button cancelButton;
 
     @FXML
-    private TextField usernameTextField;
+    public PasswordField enterPasswordField;
+
+    @FXML
+    public Button loginButton;
+
+    @FXML
+    public Label loginMessageLabel;
+
+    @FXML
+    public TextField usernameTextField;
 
     @FXML
     void cancelButtonOnAction(ActionEvent event) {
-
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
     void loginButtonOnAction(ActionEvent event) {
+        if (!usernameTextField.getText().isEmpty() &&
+                !enterPasswordField.getText().isEmpty()) {
+            checkLogin();
+        } else {
+            loginMessageLabel.setTextFill(Color.RED);
+            loginMessageLabel.setText("Please enter a valid username/password");
+        }
+    }
+
+    public void checkLogin() {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File brandingFile = new File("images/Logo.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+    }
 }
