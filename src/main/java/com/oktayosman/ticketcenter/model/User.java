0a3baae0b5363fe8e13d231cf928d0a9a088f1f7
oktay-osman <oktai.osman21@gmail.com>
2025-01-17 16:16:01 +0200

    updated User model to include BCrypt for password hashing

--- src/main/java/com/oktayosman/ticketcenter/model/User.java
+++ src/main/java/com/oktayosman/ticketcenter/model/User.java
@@ -1,28 +1,58 @@
-package app.model;
+package com.oktayosman.ticketcenter.model;
 
-import app.service.PasswordEncryptor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+import org.mindrot.jbcrypt.BCrypt;
+
+
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
-    public User(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = PasswordEncryptor.encryptPassword(password);
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
 
     public User() {
-
     }
 
-    public int getId() {
+    public User(String firstName, String lastName, String email, String username, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = hashPassword(password);
+        this.role = role;
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+
+    public Long getId() {
         return id;
     }
 
@@ -39,6 +69,22 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = PasswordEncryptor.encryptPassword(password);
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+    }
+
+    private boolean verifyPassword (String plainPassword) {
+        return BCrypt.checkpw(plainPassword, this.password);
     }
 }
