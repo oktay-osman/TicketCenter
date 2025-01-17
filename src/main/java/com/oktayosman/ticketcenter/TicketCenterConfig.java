c41228ca196233e520efaa72515c19a2cb0ba54e
oktay-osman <oktai.osman21@gmail.com>
2025-01-17 16:23:20 +0200

    added TicketCenterConfig for centralized application configuration

---
+++ src/main/java/com/oktayosman/ticketcenter/TicketCenterConfig.java
@@ -0,0 +1,7 @@
+package com.oktayosman.ticketcenter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.oktayosman.ticketcenter")
+public class TicketCenterConfig {
+}
