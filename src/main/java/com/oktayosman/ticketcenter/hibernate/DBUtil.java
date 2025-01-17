507e47d822af28305a92e9d99f1d6cba7d706edf
oktay-osman <oktai.osman21@gmail.com>
2025-01-17 16:26:56 +0200

    added DBUtil class for Hibernate Session Factory management

---
+++ src/main/java/com/oktayosman/ticketcenter/hibernate/DBUtil.java
@@ -0,0 +1,20 @@
+package com.oktayosman.ticketcenter.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DBUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed: " + ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
