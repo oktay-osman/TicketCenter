7977af2b6683c23189c84befb799825a16c1f01f
oktay-osman <oktai.osman21@gmail.com>
2025-01-21 10:23:23 +0200

    added @Repository annotation for Spring

--- src/main/java/com/oktayosman/ticketcenter/repository/UserRepository.java
+++ src/main/java/com/oktayosman/ticketcenter/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.oktayosman.ticketcenter.repository;
 
 import com.oktayosman.ticketcenter.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserByUsername(String username);
 }
