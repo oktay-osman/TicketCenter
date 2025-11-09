471925a32377aea582fbafe149f551b31026a10a
oktay-osman <oktai.osman21@gmail.com>
2025-11-09 23:21:15 +0200

    Refactor: wrap repository find methods in Optional to avoid null handling

--- src/main/java/com/oktayosman/ticketcenter/repository/RoleRepository.java
+++ src/main/java/com/oktayosman/ticketcenter/repository/RoleRepository.java
@@ -2,7 +2,11 @@ package com.oktayosman.ticketcenter.repository;
 
 import com.oktayosman.ticketcenter.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
+    Optional<Role> findByName(String name);
 }
